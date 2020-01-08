4e8a35b26a23f1180634501de4c7c21c06fc72a4
Ana Micu <Pyaeth@users.noreply.github.com>
2020-01-08 08:29:38 +0200

    Update dashboard-doughnut-chart.component.ts

--- src/components/dashboard-doughnut-chart/dashboard-doughnut-chart.component.ts
+++ src/components/dashboard-doughnut-chart/dashboard-doughnut-chart.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardDoughnutChartComponent implements OnInit {
   public doughnutChartLabels = ['Food/Restaurant', 'Shopping', 'Online Payments', 'Bills', 'Bank Credits', 'Insurance Policies'];
-  public doughnutChartData = [120, 150, 180, 90, 50];
+  public doughnutChartData = [120, 150, 180, 90, 101, 98];
   public doughnutChartType = 'doughnut';
   constructor() { }
 
